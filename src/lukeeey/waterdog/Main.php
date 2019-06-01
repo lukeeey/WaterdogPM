4575dec6f9f25e9ba48a6020331e01ffa9a3dc57
Tobias G <39413320+TobiasG-DE@users.noreply.github.com>
2019-06-01 17:42:23 +0200

    Remove PacketLimit from PMMP Interfaces

--- src/lukeeey/waterdog/Main.php
+++ src/lukeeey/waterdog/Main.php
@@ -1,11 +1,15 @@
 <?php
-
+declare ( strict_types = 1 );
 namespace lukeeey\waterdog;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\RakLibInterface;
+use ReflectionProperty;
+use ReflectionException;
+use ReflectionClass;
 
 class Main extends PluginBase implements Listener {
 
@@ -15,9 +19,18 @@ class Main extends PluginBase implements Listener {
 
     public function onPacketReceive(DataPacketReceiveEvent $event): void {
         $packet = $event->getPacket();
-        if($packet instanceof LoginPacket) {
+        if($packet instanceof LoginPacket) {   
+                foreach ( $this->getServer()->getNetwork()->getInterfaces() as $interface ) {
+						  if ( $interface instanceof RakLibInterface ) {
+								try {
+									 $reflector = new \ReflectionProperty( $interface, "interface" );
+									 $reflector->setAccessible( true );
+									 $reflector->getValue( $interface )->sendOption( "packetLimit", 900000000000 );
+								} catch ( ReflectionException $e ) {}
+						  }
+					 }
             if(isset($packet->clientData["WaterDog_RemoteIP"])) {
-                $class = new \ReflectionClass($event->getPlayer());
+                $class = new ReflectionClass($event->getPlayer());
 
                 $prop = $class->getProperty("ip");
                 $prop->setAccessible(true);
